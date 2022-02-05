266f7a9adc427b98df94fe71c40353d6e00da64f
zannabianca1997 <zannabianca199712@gmail.com>
2022-02-05 23:34:33 +0100

    Setted up for test discovery

---
+++ src/misc/test_discovery.py
@@ -0,0 +1,3 @@
+#!/bin/env python3
+from sys import argv
+print(argv)
---
+++ src/test_tests.c
@@ -0,0 +1,13 @@
+/*
+    Test file to test the test discovery
+*/
+
+// this test will always success
+int test_success(){
+    return 1;
+}
+
+// this test will always fail
+int test_fail(){
+    return 0;
+}
