525b97dca5a866de62987e4f610a333d91249525
zannabianca1997 <zannabianca199712@gmail.com>
2022-02-08 19:12:41 +0100

    added testing of line splitting

--- src/preprocessor/test_lines.c
+++ src/preprocessor/test_lines.c
@@ -6,65 +6,113 @@
 #include "lines.h"
 #include "linesco.h"
 
+#define REPORT_MSG_MAX_LEN 256
+#define LINES_BUF_MAX_LEN 256
+
+static const char *const TEST_LINES[] = {
+    "Line1",
+    "This is line 2 ",
+    " This is another line",
+    "",
+    "Oh no, it's still going?",
+    "Please kill me",
+    NULL};
+
+static const struct
+{
+    char name[5];
+    char nl[3];
+} NEWLINES[] = {{"\\n", "\n"},
+                {"\\r", "\r"},
+                {"\\r\\n", "\r\n"},
+                {"", ""}};
+
+static inline size_t total_line_len()
+{
+    size_t total = 0;
+    for (size_t i = 0; TEST_LINES[i] != NULL; i++)
+        total += strlen(TEST_LINES[i]);
+    return total;
+}
+static inline size_t num_lines()
+{
+    size_t total = 0;
+    for (size_t i = 0; TEST_LINES[i] != NULL; i++)
+        total++;
+    return total;
+}
+
+static char linebuf[LINES_BUF_MAX_LEN + 1];
+static char report_msg[REPORT_MSG_MAX_LEN + 1];
+
 const char *test_rawlines()
 {
-    //TODO: write this mess in a more streamlined way
-    //      maybe referencing a catalog in some way
-    char *rawlines = "Row1\nRow2 foo\nRow3 baz \n";
-    FILE *rawlines_file = fmemopen(rawlines, strlen(rawlines), "r");
+    context_t *testing_c = context_new(NULL, "testing rawlines splitting");
 
-    linestream_t *lstream = linestream_open(NULL, rawlines_file);
+    // testing for all possible newlines
+    for (size_t i = 0; NEWLINES[i].name[0] != '\0'; i++)
+    {
+        // start typing report
+        char *report_msg_p1 = report_msg;
+        report_msg_p1 += snprintf(report_msg_p1, REPORT_MSG_MAX_LEN - (report_msg_p1 - report_msg), "Testing newline '%s': ", NEWLINES[i].name);
 
-    struct logical_line_s *line;
+        // preparing test case
+        linebuf[0] = '\0';
+        strcat(linebuf, TEST_LINES[0]);
+        for (size_t j = 1; TEST_LINES[j] != NULL; j++)
+        {
+            strcat(linebuf, NEWLINES[i].nl);
+            strcat(linebuf, TEST_LINES[j]);
+        }
+        FILE *input = fmemopen(linebuf, strlen(linebuf), "r");
 
-    line = linestream_get(NULL, lstream);
+        // creating a line stream
+        linestream_t *lstream = linestream_open(testing_c, input);
 
-    if (line == NULL)
-        return "First line wasn't produced";
-    if (line->index[0].row != 1 || line->index[0].start != 0)
-        return "First line is misplaced!";
-    if (line->index[1].row != 0)
-        return "First line is composed of more than one line!";
-    if (strcmp(line->content, "Row1"))
-        return "First line is inexact";
+        // iterate to the end of the stream
+        size_t line_count = 0;
+        for (struct logical_line_s *ll = linestream_get(testing_c, lstream); ll != NULL; line_free(ll), ll = linestream_get(testing_c, lstream))
+        {
+            line_count++;
 
-    line = linestream_get(NULL, lstream);
+            char *report_msg_p2 = report_msg_p1;
+            report_msg_p2 += snprintf(report_msg_p2, REPORT_MSG_MAX_LEN - (report_msg_p2 - report_msg), "Line %lu ", line_count);
 
-    if (line == NULL)
-        return "Second line wasn't produced";
-    if (line->index[0].row != 2 || line->index[0].start != 0)
-        return "Second line is misplaced!";
-    if (line->index[1].row != 0)
-        return "Second line is composed of more than one line!";
-    if (strcmp(line->content, "Row2 foo"))
-        return "Second line is inexact";
+            // check line count
+            if (ll->index[0].row != line_count)
+            {
+                snprintf(report_msg_p2, REPORT_MSG_MAX_LEN - (report_msg_p2 - report_msg), "was reported as line %lu", ll->index[0].row);
+                return report_msg;
+            }
+            // check line start
+            if (ll->index[0].start != 0)
+            {
+                snprintf(report_msg_p2, REPORT_MSG_MAX_LEN - (report_msg_p2 - report_msg), "started at %lu instead of 0", ll->index[0].start);
+                return report_msg;
+            }
+            // check is a single line
+            if (ll->index[1].row != 0)
+            {
+                size_t rl_count = 1;
+                while (ll->index[rl_count].row != 0)
+                    rl_count++;
 
-    line = linestream_get(NULL, lstream);
-
-    if (line == NULL)
-        return "Third line wasn't produced";
-    if (line->index[0].row != 3 || line->index[0].start != 0)
-        return "Third line is misplaced!";
-    if (line->index[1].row != 0)
-        return "Third line is composed of more than one line!";
-    if (strcmp(line->content, "Row3 baz "))
-        return "Third line is inexact";
-
-    line = linestream_get(NULL, lstream);
-
-    if (line == NULL)
-        return "Fourth (empty) line wasn't produced";
-    if (line->index[0].row != 4 || line->index[0].start != 0)
-        return "Fourth (empty) line is misplaced!";
-    if (line->index[1].row != 0)
-        return "Fourth (empty) line is composed of more than one line!";
-    if (strcmp(line->content, ""))
-        return "Fourth (empty) line is inexact";
-
-    line = linestream_get(NULL, lstream);
-
-    if (line != NULL)
-        return "A fifth line (not in the input) was produced!";
-
+                snprintf(report_msg_p2, REPORT_MSG_MAX_LEN - (report_msg_p2 - report_msg), "has %lu raw lines inside (1 expected)", rl_count);
+                return report_msg;
+            }
+            // check we did not go over the limit
+            if (TEST_LINES[line_count - 1] == NULL)
+            {
+                snprintf(report_msg, REPORT_MSG_MAX_LEN, "Exceeded %lu lines, first unexpected line has content \"%s\"", line_count, ll->content);
+                return report_msg;
+            }
+            // check content
+            if (strcmp(ll->content, TEST_LINES[line_count - 1]) != 0)
+            {
+                snprintf(report_msg_p2, REPORT_MSG_MAX_LEN - (report_msg_p2 - report_msg), "was expected to be \"%s\", found instead \"%s\"", TEST_LINES[line_count], ll->content);
+                return report_msg;
+            }
+        }
+    }
     return NULL;
 }
