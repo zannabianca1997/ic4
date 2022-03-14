d2d78bad8a0012c4e722dfede0b3a7ddb859b6a4
zannabianca1997 <zannabianca199712@gmail.com>
2022-03-14 20:41:19 +0100

    moved punctuatoe table to header

--- src/preprocessor/tokenizer.h
+++ src/preprocessor/tokenizer.h
@@ -4,10 +4,10 @@
  * @brief Generate a stream of preprocessing tokens
  * @version 0.1
  * @date 2022-02-10
- * 
- * Interface to tokenizer.c main functionalities: 
+ *
+ * Interface to tokenizer.c main functionalities:
  * generation of a stream of preprocessing tokens.
- * 
+ *
  * @copyright Copyright (c) 2022
  */
 
@@ -24,12 +24,12 @@
 
 /**
  * @brief Contain all data relative to a single preprocesso token
- * 
+ *
  */
 struct pp_token_s
 {
     /**
-     * @brief The type of the token 
+     * @brief The type of the token
      */
     enum pp_tok_type_e
     {
@@ -40,7 +40,7 @@ struct pp_token_s
         PP_TOK_STRING_LIT, // string literals
         PP_TOK_CHAR_CONST, // char constants
         PP_TOK_HEADER,     // Header names
-        PP_TOK_MACRO_NAME,     // macro names
+        PP_TOK_MACRO_NAME, // macro names
         PP_TOK_PUNCTUATOR, // punctuators
 
         // In-Band structure info
@@ -76,7 +76,7 @@ struct pp_token_s
         // macro names
         struct
         {
-            char *name;     // the name of the macro
+            char *name;       // the name of the macro
             bool is_function; // if the macro is function-like
         } macro_name;
 
@@ -115,12 +115,12 @@ struct pp_token_s
 
             // bitwise
 
-            PUNC_BIT_NOT,    // bitwise not
-            PUNC_BIT_AND_OR_REFTO,    // bitwise and
-            PUNC_BIT_OR,     // bitwise shift
-            PUNC_BIT_XOR,    // bitwise xor
-            PUNC_BIT_LSHIFT, // bit left shift
-            PUNC_BIT_RSHIFT, // bit right shift
+            PUNC_BIT_NOT,          // bitwise not
+            PUNC_BIT_AND_OR_REFTO, // bitwise and
+            PUNC_BIT_OR,           // bitwise shift
+            PUNC_BIT_XOR,          // bitwise xor
+            PUNC_BIT_LSHIFT,       // bit left shift
+            PUNC_BIT_RSHIFT,       // bit right shift
 
             // assignements
 
@@ -177,6 +177,102 @@ struct pp_token_s
     };
 };
 
+#define PUNCTUATOR_LENGTH 3
+
+/**
+ * @brief Table of punctuators.
+ *
+ * Connect the text of a punctuator to its enum value
+ * Terminated by a empty text
+ */
+static const struct
+{
+    char str[PUNCTUATOR_LENGTH + 1]; // Text of the punctuator
+    enum punctuator_e punc;          // Enumeration value of the punctuator
+} PUNCTUATORS_STRINGS[] = {
+    // -- OPERATORS
+
+    // aritmetic
+
+    {"+", PUNC_ADD},          // add
+    {"-", PUNC_SUB},          // subtract
+    {"*", PUNC_MUL_OR_DEREF}, // multiply or pointer dereferencing
+    {"/", PUNC_DIV},          // divide
+    {"%", PUNC_MOD},          // modulus
+
+    // comparators
+
+    {"==", PUNC_EQ},      // equal to
+    {"!=", PUNC_NEQ},     // not equal to
+    {"<", PUNC_LESS},     // less than
+    {"<=", PUNC_LESSEQ},  // less or equal than
+    {">", PUNC_GREAT},    // greater than
+    {">=", PUNC_GREATEQ}, // greater or equal than
+
+    // logical
+
+    {"!", PUNC_NOT},      // logical not
+    {"&&", PUNC_AND},     // logical and
+    {"||", PUNC_OR},      // logical or
+    {"?", PUNC_QUESTION}, // question mark (for ternary logical)
+    {":", PUNC_COLON},    // colon (for ternary logic and bit fields)
+
+    // bitwise
+
+    {"~", PUNC_BIT_NOT},          // bitwise not
+    {"&", PUNC_BIT_AND_OR_REFTO}, // bitwise and or reference to
+    {"|", PUNC_BIT_OR},           // bitwise shift
+    {"^", PUNC_BIT_XOR},          // bitwise xor
+    {"<<", PUNC_BIT_LSHIFT},      // bit left shift
+    {">>", PUNC_BIT_RSHIFT},      // bit right shift
+
+    // assignements
+
+    {"=", PUNC_ASSIGN},              // assign to
+    {"+=", PUNC_ADD_ASSIGN},         // sub and assign to
+    {"-=", PUNC_SUB_ASSIGN},         // subtract and assign to
+    {"*=", PUNC_MUL_ASSIGN},         // multiply and assign to
+    {"/=", PUNC_DIV_ASSIGN},         // divide and assign to
+    {"%=", PUNC_MOD_ASSIGN},         // modulus and assign to
+    {"&=", PUNC_BIT_AND_ASSIGN},     // bitwise and and assign to
+    {"|=", PUNC_BIT_OR_ASSIGN},      // bitwise or and assign to
+    {"^=", PUNC_BIT_XOR_ASSIGN},     // bitwise xor and assign to
+    {"<<=", PUNC_BIT_LSHIFT_ASSIGN}, // bitwise left shift and assign to
+    {">>=", PUNC_BIT_RSHIFT_ASSIGN}, // bitwise right shift and assign to
+
+    // augment and decrease
+
+    {"++", PUNC_AUGMENT}, // augment
+    {"--", PUNC_DECR},    // decrease
+
+    // -- PARENTHESES
+
+    {"(", PUNC_PAR_LEFT},     // left parenthese
+    {")", PUNC_PAR_RIGHT},    // right parenthese
+    {"[", PUNC_SQRPAR_LEFT},  // left square parenthese
+    {"]", PUNC_SQRPAR_RIGHT}, // rigth square parenthese
+    {"{", PUNC_CURPAR_LEFT},  // left curly parenthese
+    {"}", PUNC_CURPAR_RIGHT}, // rigth curly parenthese
+
+    // -- POINTERS AND STRUCTS
+
+    // PUNC_DEREF -> see under aritmetic
+    // PUNC_REFTO -> see under bitwise
+    {".", PUNC_MEMBER_ACCESS},      // member access
+    {"->", PUNC_IND_MEMBER_ACCESS}, // indirect member access
+
+    // -- SEPARATORS
+
+    {",", PUNC_COMMA},   // comma operator or argument separator
+    {";", PUNC_SEMICOL}, // end of statement
+
+    // -- PREPROCESSOR
+
+    {"#", PUNC_STRINGIZE}, // stringize
+    {"##", PUNC_TOKPASTE}, // token pasting
+
+    {"", 0}}; // Terminator
+
 /**
  * @brief Deallocate memory bound to token
  * Deallocate memory bound to token (e.g. string content or identifiers names)
@@ -186,9 +282,9 @@ void pp_tok_free(struct pp_token_s *token);
 
 /**
  * @brief Compare two tokens.
- * 
+ *
  * Do not check token.mark: comparation is position indipendent
- * 
+ *
  * @param a one of the token to compare
  * @param b the other token to compare
  * @return true the two token are equal
@@ -200,13 +296,13 @@ bool pp_tok_cmp(struct pp_token_s const *a, struct pp_token_s const *b);
 
 /**
  * @brief Contain all data regarding a token stream
- * 
+ *
  */
 typedef struct pp_tokstream_s pp_tokstream_t;
 
 /**
  * @brief Get the next token in the stream
- * 
+ *
  * @param context the context for which the token is needed
  * @param stream the stream from which the token is taken
  * @return struct pp_token_s* the taken token, or NULL if source has exausted
@@ -215,9 +311,9 @@ struct pp_token_s *pp_tokstream_get(context_t *context, pp_tokstream_t *stream);
 
 /**
  * @brief Put a token back on the stream to be collected after.
- * 
+ *
  * A maximum of a single token can be ungetted
- * 
+ *
  * @param stream the stream on which the token is put
  * @param token the token to unget
  */
@@ -225,10 +321,10 @@ void pp_tokstream_unget(pp_tokstream_t *stream, struct pp_token_s *token);
 
 /**
  * @brief Close a token stream
- * 
+ *
  * Close a token stream, freeing the resources allocates with it.
  * If recursive_close is true, the underlying stream is closed too, and instructed to close it's source
- * 
+ *
  * @param stream the stream to close
  * @param recursive_close if stream closure is to propagate
  */
