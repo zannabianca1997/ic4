37b0985bc47c5c441e377c732e32c3cfb69459a9
zannabianca1997 <zannabianca199712@gmail.com>
2022-03-14 22:57:27 +0100

    fixed forgot rename

--- src/preprocessor/interpreterco.h
+++ src/preprocessor/interpreterco.h
@@ -18,9 +18,9 @@
  * @brief Create a new interpreter, reading a translation unit
  *
  * @param context the context creating the interpreter
- * @param tsunit the translation unit to interpret
+ * @param file the starting file of the unit
  * @return pp_interpreter_t* the interpreter opened
  */
-pp_interpreter_t *pp_interpreter_open(context_t *context, pp_tsunit_t *tsunit);
+pp_interpreter_t *pp_interpreter_open(context_t *context, pp_file_t *tsunit);
 
 #endif // _INTERPRETERCO_H
