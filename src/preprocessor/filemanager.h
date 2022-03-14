ac229a862ff125ff57d667476c6adc1c87c422e6
zannabianca1997 <zannabianca199712@gmail.com>
2022-03-14 22:51:39 +0100

    documented parameter

--- src/preprocessor/filemanager.h
+++ src/preprocessor/filemanager.h
@@ -52,7 +52,17 @@ void pp_tsunit_close(pp_tsunit_t *tsunit, bool recursive_close);
  * @param fopen_f the funtion used to open a file
  * @return pp_filemanager_t* the opened file manager
  */
-pp_filemanager_t *pp_filemanager_open(context_t *context, FILE *(*fopen_f)(context_t *context, char *fname, bool is_angled));
+pp_filemanager_t *pp_filemanager_open(
+    context_t *context,
+    /**
+     * @brief The funtion used to open a file
+     *
+     * @param context the context in which the file is needed
+     * @param fname the name of the file
+     * @param is_angled if the file is to search in angled mode (internal header are included)
+     * @return FILE * a readable stream pointed to the start of the file
+     */
+    FILE *(*fopen_f)(context_t *context, char const *fname, bool is_angled));
 
 /**
  * @brief open a translation unit
