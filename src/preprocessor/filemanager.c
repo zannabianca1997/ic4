16b275003c303ab935843c124aa3432b01ebc2ca
zannabianca1997 <zannabianca199712@gmail.com>
2022-03-14 23:02:32 +0100

    atarted implementinf filemanager

---
+++ src/preprocessor/filemanager.c
@@ -0,0 +1,24 @@
+/**
+ * @file filemanager.c
+ * @author zannabianca1997 (zannabianca199712@gmail.com)
+ * @brief Manage the file opens
+ * @version 0.1
+ * @date 2022-03-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ * Read the file requested.
+ * Mark the directive with the file from which they came.
+ * When asked again for the same file, it won't reread it.
+ *
+ */
+
+#include "filemanager.h"
+
+struct pp_filemanager_s
+{
+};
+
+struct pp_file_s
+{
+};
