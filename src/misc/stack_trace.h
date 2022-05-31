b5a6e5c97048ddcba02622e6ad985a9fb92b245d
zannabianca1997 <zannabianca199712@gmail.com>
2022-05-31 17:51:59 +0200

    added stack trace file (old contexts)

---
+++ src/misc/stack_trace.h
@@ -0,0 +1,10 @@
+/**
+ * @file stack_trace.h
+ * @author zannabianca1997 (zannabianca199712@gmail.com)
+ * @brief Give some basic tracing utilities
+ * @version 0.1
+ * @date 2022-05-31
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
