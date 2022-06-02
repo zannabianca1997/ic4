08e9b414ffe5163f5ec83defa1315c106e9dbd7f
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-02 13:58:56 +0200

    fixed missing status return

--- tests/test_tap.c
+++ tests/test_tap.c
@@ -2,18 +2,14 @@
 
 #include "tap.h"
 
-static void default_putc(void *stream, char ch) { putc(ch, (FILE *)stream); }
-
 int main(int argc, char const *argv[])
 {
-    set_output(&default_putc, (void *)stdout);
-
     plan(2, NULL);
-    ok(3 < 2, "basic comparison");
+    ok(3 > 2, "basic comparison");
 
     todo("Some todo");
     fail("Oh no...");
     end_todo();
 
-    return 0;
+    return exit_status();
 }
