c2e0775918955dd15a1ee0d1cf8bbf32c3fb2fce
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-15 17:18:49 +0200

    Expression data types

--- src/assembler/expressions.py
+++ src/assembler/expressions.py
@@ -2,8 +2,20 @@
     Methods to manipulate arithmetic expressions
 """
 from dataclasses import dataclass
+from typing import Tuple, Union
 
+# A single, atomic factor: a number, a identifier,
+# or a parentisized expression.
+Factor = Union[int, str, 'Expression']
 
-@dataclass(frozen=True)
-class Expression:
-    pass
+# A product of factors
+Term = Tuple[
+    Tuple[bool, Factor],  # first element is true if it's multiplied
+    ...
+]
+
+# A sum of terms
+Expression = Tuple[
+    Tuple[bool, Term],  # first element is true if it's divided
+    ...
+]
