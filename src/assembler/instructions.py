8758c72d1b0a2fb4fa35b6673b79a132ceb17f73
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-15 18:20:39 +0200

    Added table of number of parameters

--- src/assembler/instructions.py
+++ src/assembler/instructions.py
@@ -13,12 +13,25 @@ class OpCode(IntEnum):
     MUL = 2
     IN = 3
     OUT = 4
-    JZ = 5
-    JNZ = 6
+    JNZ = 5
+    JZ = 6
     SLT = 7
     SEQ = 8
     INCB = 9
     HALT = 99
+
+    def param_number(self) -> int:
+        return {OpCode.ADD: 3,
+                OpCode.MUL: 3,
+                OpCode.IN: 1,
+                OpCode.OUT: 1,
+                OpCode.JZ: 2,
+                OpCode.JNZ: 2,
+                OpCode.SLT: 3,
+                OpCode.SEQ: 3,
+                OpCode.INCB: 1,
+                OpCode.HALT: 0
+                }[self]
 
 
 class ParamMode(IntEnum):
