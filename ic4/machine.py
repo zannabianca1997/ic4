535ff87f38ddfc691fd792eb6433bb1367bb5fef
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-17 18:15:58 +0200

    fixed wrong machine indexing

--- ic4/machine.py
+++ ic4/machine.py
@@ -28,12 +28,8 @@ class Machine:
         self._input = []
         self._output = []
 
-    def _get_mem(self, idx: int, parmode: int) -> int:
+    def _get_mem(self, idx: int) -> int:
         """Get memory at index"""
-        if parmode == 1:
-            return idx  # immediate mode
-        if parmode == 2:
-            idx += self._RB
         if idx < 0:
             raise IndexError(
                 f"Accessed negative memory location {idx} for reading")
@@ -42,12 +38,8 @@ class Machine:
         else:
             return self._memory[idx]
 
-    def _set_mem(self, idx: int, parmode: int, val: int) -> None:
+    def _set_mem(self, idx: int, val: int) -> None:
         """Set memory at a index"""
-        if parmode == 1:
-            raise ValueError(f"Cannot use immediate mode for writing")
-        if parmode == 2:
-            idx += self._RB
         if idx < 0:
             raise IndexError(
                 f"Accessed negative memory location {idx} for writing")
@@ -59,12 +51,31 @@ class Machine:
         else:
             self._memory[idx] = val
 
+    def _get_param(self, idx: int, mode: int) -> int:
+        if mode == 0:
+            return self._get_mem(self._get_mem(idx))
+        if mode == 1:
+            return self._get_mem(idx)
+        if mode == 2:
+            return self._get_mem(self._RB+self._get_mem(idx))
+        raise ValueError(f"Invalid param mode {mode}")
+
+    def _set_param(self, idx: int, mode: int, val: int) -> None:
+        if mode == 0:
+            self._set_mem(self._get_mem(idx), val)
+        elif mode == 1:
+            raise ValueError(f"Param mode 1 cannot be used for writing!")
+        elif mode == 2:
+            self._set_mem(self._RB+self._get_mem(idx), val)
+        else:
+            raise ValueError(f"Invalid param mode {mode}")
+
     def run(self) -> bool:
         """Run the machine until halted or input is needed. return True if not halted"""
         if self._halted:
             return False
         while True:
-            op = self._get_mem(self._PC, 0)
+            op = self._get_mem(self._PC)
             opcode = op % 100
             parmode1 = (op // 100) % 10
             parmode2 = (op // 1000) % 10
@@ -79,67 +90,67 @@ class Machine:
                 raise ValueError(
                     f"{parmode3} is not an appropriate parameter mode for parameter 3")
             if opcode == 1:
-                self._set_mem(
+                self._set_param(
                     self._PC + 3, parmode3,
-                    self._get_mem(self._PC+1, parmode1) +
-                    self._get_mem(self._PC+2, parmode2)
+                    self._get_param(self._PC+1, parmode1) +
+                    self._get_param(self._PC+2, parmode2)
                 )
                 self._PC += 4
             elif opcode == 2:
-                self._set_mem(
+                self._set_param(
                     self._PC + 3, parmode3,
-                    self._get_mem(self._PC+1, parmode1) *
-                    self._get_mem(self._PC+2, parmode2)
+                    self._get_param(self._PC+1, parmode1) *
+                    self._get_param(self._PC+2, parmode2)
                 )
                 self._PC += 4
             elif opcode == 3:
                 if not self._input:
                     return True  # wait for more input
-                self._set_mem(self._PC+1, parmode1, self._input.pop(0))
+                self._set_param(self._PC+1, parmode1, self._input.pop(0))
                 self._PC += 2
             elif opcode == 4:
                 self._output.append(
-                    self._get_mem(self._PC+1, parmode1))
+                    self._get_param(self._PC+1, parmode1))
                 self._PC += 2
             elif opcode == 5:
-                if self._get_mem(self._PC+1, parmode1):
-                    self._PC = self._get_mem(self._PC+2, parmode2)
+                if self._get_param(self._PC+1, parmode1):
+                    self._PC = self._get_param(self._PC+2, parmode2)
                 else:
                     self._PC += 3
             elif opcode == 6:
-                if not self._get_mem(self._PC+1, parmode1):
-                    self._PC = self._get_mem(self._PC+2, parmode2)
+                if not self._get_param(self._PC+1, parmode1):
+                    self._PC = self._get_param(self._PC+2, parmode2)
                 else:
                     self._PC += 3
             elif opcode == 7:
-                self._set_mem(
+                self._set_param(
                     self._PC + 3, parmode3,
-                    1 if (self._get_mem(self._PC+1, parmode1) <
-                          self._get_mem(self._PC+2, parmode2)) else 0
+                    1 if (self._get_param(self._PC+1, parmode1) <
+                          self._get_param(self._PC+2, parmode2)) else 0
                 )
                 self._PC += 4
             elif opcode == 8:
-                self._set_mem(
+                self._set_param(
                     self._PC + 3, parmode3,
-                    1 if (self._get_mem(self._PC+1, parmode1) ==
-                          self._get_mem(self._PC+2, parmode2)) else 0
+                    1 if (self._get_param(self._PC+1, parmode1) ==
+                          self._get_param(self._PC+2, parmode2)) else 0
                 )
                 self._PC += 4
             elif opcode == 9:
-                self._RB += self._get_mem(self._PC+1, parmode1)
+                self._RB += self._set_param(self._PC+1, parmode1)
                 self._PC += 2
             elif opcode == 99:
                 self._halted = True
                 return False
             else:
-                raise ValueError(f"Unknow opcode {opcode}")
+                raise ValueError(f"Unknow opcode {opcode} at place {self._PC}")
 
     def give_input(self, inp: Union[int, Iterable[int]]) -> None:
         """Add input to the machine"""
-        if isinstance(input, int):
-            self._input.append(input)
+        if isinstance(inp, int):
+            self._input.append(inp)
         else:
-            self._input.extend(input)
+            self._input.extend(inp)
 
     def get_output(self) -> Optional[int]:
         """Read output from the machine"""
