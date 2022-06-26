0234e55f7e4659bf72a1ce97c7c63210259206ee
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-26 11:51:06 +0200

    Added basic expression parsing

---
+++ ic4/assembler/parser.py
@@ -0,0 +1,45 @@
+"""
+Parse an assembly file
+"""
+from sly import Parser
+
+from .expressions import Divide, Multiply, Subtract, Sum
+from .lexer import ICAssLexer
+
+
+class ICAssParser(Parser):
+    tokens = ICAssLexer.tokens
+
+    precedence = (
+        ("left", PLUS, MINUS),
+        ("left", TIMES, DIVIDE),
+        ("right", UMINUS),  # Unary minus operator
+    )
+
+    @_("expr PLUS expr")
+    def expr(self, p):
+        return Sum(p.expr0, p.expr1)
+
+    @_("expr MINUS expr")
+    def expr(self, p):
+        return Subtract(p.expr0, p.expr1)
+
+    @_("expr TIMES expr")
+    def expr(self, p):
+        return Multiply(p.expr0, p.expr1)
+
+    @_("expr DIVIDE expr")
+    def expr(self, p):
+        return Divide(p.expr0, p.expr1)
+
+    @_("MINUS expr %prec UMINUS")
+    def expr(self, p):
+        return Multiply(p.expr, -1)
+
+    @_("NUMBER")
+    def expr(self, p):
+        return p.NUMBER
+
+    @_("LPAREN expr RPAREN")
+    def expr(self, p):
+        return p.expr
--- tests/assembler/test_parser.py
+++ tests/assembler/test_parser.py
@@ -2,8 +2,6 @@ from io import StringIO
 from typing import Any, Iterable, Tuple
 from unittest import TestCase, skip
 
-from ply.yacc import LRParser
-from ply.lex import Lexer
 from parameterized import parameterized
 
 from ic4.assembler.expressions import Divide, Expression, Multiply, Sum
@@ -12,14 +10,10 @@ from ic4.assembler.parser import ICAssParser
 
 
 class TestParsingExpression(TestCase):
-    stream: StringIO
-    lexer: Lexer
-    parser: LRParser
-
     def setUp(self) -> None:
         self.stream = StringIO()
-        self.lexer = ICAssLexer(self.stream, debug=True)
-        self.parser = ICAssParser(debug=True, start="expression")
+        self.lexer = ICAssLexer()
+        self.parser = ICAssParser()
 
     @parameterized.expand(
         [
@@ -33,11 +27,15 @@ class TestParsingExpression(TestCase):
                 "3+ 2 *6 /24",
                 Sum(3, Divide(Multiply(2, 6), 24)),
             ),
+            (
+                "Parenthesis",
+                "(3+ 6) /24",
+                Divide(Sum(3, 6), 24),
+            ),
         ]
     )
-    @skip("Parser not implemented")
     def test_parse(self, name: str, source: str, parsed: Iterable[Expression]):
         self.stream.write(source)
         self.stream.seek(0)
 
-        self.assertEqual(self.parser.parse(lexer=self.lexer), parsed)
+        self.assertEqual(self.parser.parse(self.lexer.tokenize(source)), parsed)
