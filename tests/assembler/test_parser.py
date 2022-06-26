25962b864a9bc55a13dfba33ea942a26caf8b87a
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-26 12:05:24 +0200

    added tests for arithmetic expressions

--- tests/assembler/test_parser.py
+++ tests/assembler/test_parser.py
@@ -4,7 +4,7 @@ from unittest import TestCase, skip
 
 from parameterized import parameterized
 
-from ic4.assembler.expressions import Divide, Expression, Multiply, Sum
+from ic4.assembler.expressions import Divide, Expression, Multiply, Subtract, Sum
 from ic4.assembler.lexer import ICAssLexer
 from ic4.assembler.parser import ICAssParser
 
@@ -21,6 +21,46 @@ class TestParsingExpression(TestCase):
                 "simple addition",
                 "3+2",
                 Sum(3, 2),
+            ),
+            (
+                "long addition",
+                "3+2+1+0+1+2+3",
+                Sum(Sum(Sum(Sum(Sum(Sum(3, 2), 1), 0), 1), 2), 3),
+            ),
+            (
+                "addiction and subtraction",
+                "3-2+1+0-1+2+3",
+                Sum(Sum(Subtract(Sum(Sum(Subtract(3, 2), 1), 0), 1), 2), 3),
+            ),
+            (
+                "unitary minus",
+                "-3",
+                Multiply(3, -1),
+            ),
+            (
+                "double unitary minus",
+                "--3",
+                Multiply(Multiply(3, -1), -1),
+            ),
+            (
+                "sum to unitary minus",
+                "1 + -3",
+                Sum(1, Multiply(3, -1)),
+            ),
+            (
+                "mult to unitary minus",
+                "1 * -3",
+                Multiply(1, Multiply(3, -1)),
+            ),
+            (
+                "divide to unitary minus",
+                "1 / -3",
+                Divide(1, Multiply(3, -1)),
+            ),
+            (
+                "divide from unitary minus",
+                "-1 / 3",
+                Divide(Multiply(1, -1), 3),
             ),
             (
                 "4 operations",
