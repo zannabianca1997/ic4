c7c959d2c7b8531eed266a2c49a2cd750ecc1be7
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-27 13:30:05 +0200

    renamed function

--- ic4/assembler/__init__.py
+++ ic4/assembler/__init__.py
@@ -5,5 +5,5 @@ from .parser import ICAssParser
 from .codegen import generate
 
 
-def compile(source: str) -> Tuple[int, ...]:
+def assemble(source: str) -> Tuple[int, ...]:
     return generate(ICAssParser().parse(ICAssLexer().tokenize(source)))
