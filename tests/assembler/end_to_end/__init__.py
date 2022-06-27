4f8b9851194884f8657c339f7dfa5297b8c74d60
zannabianca1997 <zannabianca199712@gmail.com>
2022-06-27 12:44:12 +0200

    Recreated codegen testing

--- tests/assembler/end_to_end/__init__.py
+++ tests/assembler/end_to_end/__init__.py
@@ -1,4 +1,4 @@
-from io import StringIO
+from itertools import count, takewhile
 from os import getenv
 from warnings import warn
 from pathlib import Path
@@ -8,7 +8,8 @@ from unittest import TestCase
 from parameterized import parameterized
 from pydantic import BaseModel, validator, parse_file_as
 from sly.lex import Token
-from ic4.assembler.commands import Command, Directive, Instruction, Label
+from ic4.assembler.codegen import generate
+from ic4.assembler.commands import Directive, Instruction, Label
 
 from ic4.assembler.lexer import ICAssLexer
 from ic4.assembler.parser import ICAssParser
@@ -65,18 +66,20 @@ def get_name_log_path_and_source(dir: Path):
         yield source.stem, get_log_path(source), source_code
 
 
-def get_name_source_and_examples(dir: Path):
+def get_name_source_and_example(dir: Path):
     for source in get_sources(dir):
         if not source.with_suffix(".json").exists():
             warn(f"{source!s} misses a companion .json file!")
             continue
         with open(source) as source_file:
             source_code = source_file.read()
-        io_examples = parse_file_as(Tuple[IOExample, ...], source.with_suffix(".json"))
-        yield source.stem, source_code, io_examples
+        for io_example in parse_file_as(
+            Tuple[IOExample, ...], source.with_suffix(".json")
+        ):
+            yield source.stem + "_" + io_example.name, source_code, io_example
 
 
-class TestExamplePrograms(TestCase):
+class TestLexParse(TestCase):
     @parameterized.expand(get_name_log_path_and_source(__file__))
     def test_lex(self, name: str, log_path: Path, program: str) -> None:
         """Main purpose of this test is that the program must be lexed without errors.
@@ -99,3 +102,31 @@ class TestExamplePrograms(TestCase):
                     )
                 )
                 print(command, file=log_file)
+
+    @parameterized.expand(get_name_log_path_and_source(__file__))
+    def test_compile(self, name: str, log_path: Path, program: str) -> None:
+        """Main purpose of this test is that the program must be compiled without errors.
+        As a side effect, the compiled program is logged"""
+        compiled = generate(ICAssParser().parse(ICAssLexer().tokenize(program)))
+        with open(log_path / "compiled.txt", "w") as log_file:
+            print(*compiled, sep=", ", end="", file=log_file)
+        for x in compiled:
+            self.assertIsInstance(x, int)
+
+
+class TestRun(TestCase):
+    @parameterized.expand(get_name_source_and_example(__file__))
+    def test_run(self, name: str, program: str, io_example: IOExample):
+        """Compile the program and test it against the given example input/output"""
+        machine = Machine(generate(ICAssParser().parse(ICAssLexer().tokenize(program))))
+        machine.give_input(io_example.input)
+        machine.run()
+        self.assert_(machine.halted, "The machine did not halt with the given input!")
+        self.assertTupleEqual(
+            io_example.output,
+            tuple(
+                takewhile(
+                    lambda x: x is not None, (machine.get_output() for _ in count())
+                )
+            ),
+        )
