0c924392cd0147a147ee689785e038badeee7f61
zannabianca1997 <zannabianca199712@gmail.com>
2022-07-03 15:51:13 +0200

    Reritten commands

--- ic4/assembly/commands.py
+++ ic4/assembly/commands.py
@@ -2,16 +2,16 @@
     Define basic instruction datatype
 """
 from dataclasses import dataclass
-from enum import IntEnum, Enum, auto
-from typing import Tuple, Optional, Union
+from enum import IntEnum, unique
+from typing import Optional, Tuple
 
-from py import code
-from yaml import DirectiveToken
-
-from .expressions import Expression, simplify
+from .expressions import Expression, Reference, SimplifyException
 
 
+@unique
 class OpCode(IntEnum):
+    """The code of an instruction"""
+
     ADD = 1
     MUL = 2
     IN = 3
@@ -23,7 +23,13 @@ class OpCode(IntEnum):
     INCB = 9
     HALT = 99
 
+    @property
     def param_number(self) -> int:
+        """Get the number of params this opcode require
+
+        :return: number of params
+        :rtype: int
+        """
         return {
             OpCode.ADD: 3,
             OpCode.MUL: 3,
@@ -37,147 +43,281 @@ class OpCode(IntEnum):
             OpCode.HALT: 0,
         }[self]
 
+    @property
+    def writes_to(self) -> Tuple[bool, ...]:
+        """Return a tuple indicating what params are used for writing (True for writing needed).
 
+        :return: the tuple
+        :rtype: Tuple[bool, ...]
+        """
+        return {
+            OpCode.ADD: (False, False, True),
+            OpCode.MUL: (False, False, True),
+            OpCode.IN: (True,),
+            OpCode.OUT: (False,),
+            OpCode.JZ: (False, False),
+            OpCode.JNZ: (False, False),
+            OpCode.SLT: (False, False, True),
+            OpCode.SEQ: (False, False, True),
+            OpCode.INCB: (False,),
+            OpCode.HALT: (),
+        }[self]
+
+
+@unique
 class ParamMode(IntEnum):
+    """Mode of a parameter"""
+
     ABSOLUTE = 0
     IMMEDIATE = 1
     RELATIVE = 2
 
+    @property
     def prefix(self) -> str:
+        """Prefix used in assembly language for this mode
+
+        :return: the prefix
+        :rtype: str
+        """
         return {
             ParamMode.ABSOLUTE: "",
             ParamMode.IMMEDIATE: "#",
             ParamMode.RELATIVE: "@",
         }[self]
 
-
-INSTRUCTION_PARAMS_WRITING_TABLE = {
-    OpCode.ADD: (False, False, True),
-    OpCode.MUL: (False, False, True),
-    OpCode.IN: (True,),
-    OpCode.OUT: (False,),
-    OpCode.JZ: (False, False),
-    OpCode.JNZ: (False, False),
-    OpCode.SLT: (False, False, True),
-    OpCode.SEQ: (False, False, True),
-    OpCode.INCB: (False,),
-    OpCode.HALT: (),
-}
+    @property
+    def can_be_written(self) -> bool:
+        return self is not ParamMode.IMMEDIATE
 
 
 @dataclass(frozen=True)
-class Instruction:
+class Param:
+    mode: ParamMode
+    value: Expression
+
+    @property
+    def can_be_written(self) -> bool:
+        return self.mode.can_be_written
+
+    def check(self) -> None:
+        """Perform some basic checks on this parameter
+
+        :raises: AssertionError if the check failed
+        """
+        if self.mode is ParamMode.ABSOLUTE:
+            # value cannot be negative
+            try:
+                val = self.value.simplify(fullsimplify=True)
+            except SimplifyException:
+                pass  # do not panic, it's simply a complex parameter
+            else:
+                assert val.value >= 0, "Absolute parameters cannot have negative values"
+
+
+@dataclass(frozen=True)
+class Command:
+    """Represent a command in a file"""
+
+    def check(self) -> None:
+        """Perform some basic checks on this command
+
+        :raises: AssertionError if the check failed
+        """
+        raise NotImplementedError()
+
+
+@dataclass(frozen=True)
+class Label(Command):
+    """Represent a label"""
+
+    value: str
+
+    def check(self) -> None:
+        return Reference.NAMES_RE.fullmatch(self.value)
+
+
+@dataclass(frozen=True)
+class Instruction(Command):
+    """Represent a instruction"""
+
     opcode: OpCode
-    params: Tuple[Tuple[ParamMode, Expression], ...]
+    params: Tuple[Param, ...]
 
-    def params_check(self, check_ranges=True, **_) -> None:
-        """Checks if all the params are valid (e.g. no immediate on writing parameters or negative on absolute)"""
-        assert (
-            len(self.params) == self.opcode.param_number()
-        ), f"Instruction {self.opcode.name} expected {self.opcode.param_number()} params, got {len(self.params)}"
-
-        # checks if a param is used for writing
-        for (mode, _), writing in zip(
-            self.params, INSTRUCTION_PARAMS_WRITING_TABLE[self.opcode]
-        ):
-            if writing:
-                assert (
-                    mode != ParamMode.IMMEDIATE
-                ), f"Instruction {self.opcode.name} do not accept IMMEDIATE mode on output parameter"
-
-        if check_ranges:
-            for mode, value in self.params:
-                if mode == ParamMode.ABSOLUTE:
-                    value = simplify(value)
-                    if isinstance(value, int):
-                        assert value >= 0, "Negative value on absolute parameter"
-
-
-class DirectiveCode(Enum):
-    INTS = auto()
-    ZEROS = auto()
-    INC = auto()
-    DEC = auto()
-    MOV = auto()
-    LOAD = auto()
-    STORE = auto()
-    JMP = auto()
-    PUSH = auto()
-    POP = auto()
-    CALL = auto()
-    RET = auto()
+    def check(self) -> None:
+        # check number of params
+        assert len(self.params, self.opcode.param_number)
+        # checks all params that are used for writing can be written
+        assert all(
+            (not writes_to) or p.can_be_written
+            for writes_to, p in zip(self.opcode.writes_to, self.params)
+        )
+        # self check of the params
+        for p in self.params:
+            p.check()
 
 
 @dataclass(frozen=True)
-class Directive:
-    code: DirectiveCode
-    params: Tuple[Union[Tuple[ParamMode, Expression], Expression], ...]
+class Directive(Command):
+    """Represent an assambler directive"""
 
-    def params_check(self, **_) -> None:
-        """Check that the parameters are of the right type"""
-        if self.code == DirectiveCode.ZEROS:
-            assert len(self.params) == 1
-            p = simplify(self.params[0])
-            if isinstance(p, int):
-                assert p >= 0
-        if self.code in {DirectiveCode.DEC, DirectiveCode.INC, DirectiveCode.JMP}:
-            assert len(self.params) == 1
-            (param,) = self.params
-            assert isinstance(param, tuple) and len(param) == 2
-            mode, _ = param
-            assert isinstance(mode, ParamMode)
-        if self.code == DirectiveCode.MOV:
-            assert len(self.params) == 3
-            p1, p2, p3 = self.params
-            for p in p1, p2:
-                assert isinstance(p, tuple) and len(p) == 2
-                mode, _ = p
-                assert isinstance(mode, ParamMode)
-            assert p2[0] != ParamMode.IMMEDIATE
-            p3 = simplify(p3)
-            if isinstance(p3, int):
-                assert p3 >= 0
-        if self.code in {DirectiveCode.LOAD, DirectiveCode.STORE}:
-            assert len(self.params) == 2
-            src, dest = self.params
-            for p in src, dest:
-                assert isinstance(p, tuple) and len(p) == 2
-                mode, _ = p
-                assert isinstance(mode, ParamMode)
-            if self.code == DirectiveCode.STORE:
-                assert dest[0] != ParamMode.IMMEDIATE
-        if self.code in {DirectiveCode.PUSH, DirectiveCode.POP}:
-            assert len(self.params) == 2
-            data, leng = self.params
-            if data is not None:
-                assert isinstance(data, tuple) and len(data) == 2
-                mode, _ = data
-                assert isinstance(mode, ParamMode)
-                if code == DirectiveCode.POP:
-                    assert mode != ParamMode.IMMEDIATE
-            leng = simplify(leng)
-            if isinstance(leng, int):
-                assert leng >= 0
-        if self.code == DirectiveCode.CALL:
-            assert len(self.params) == 1
-            (p,) = self.params
-            assert isinstance(p, tuple) and len(p) == 2
-            mode, addr = p
-            assert isinstance(mode, ParamMode)
-            if mode != ParamMode.RELATIVE:
-                addr = simplify(addr)
-                if isinstance(addr, int):
-                    assert addr >= 0
-        if self.code == DirectiveCode.RET:
-            assert len(self.params) == 0
+    pass
 
 
 @dataclass(frozen=True)
-class Label:
-    name: str
+class INTS(Directive):
+    """Insert the given numbers verbatim"""
 
-    def params_check(self, **_) -> None:
+    values: Tuple[Expression, ...]
+
+    def check(self) -> None:
         pass
 
 
-Command = Union[Label, Directive, Instruction]
+@dataclass(frozen=True)
+class ZEROS(Directive):
+    """Insert a given number of zero"""
+
+    len: Expression
+
+    def check(self) -> None:
+        # len cannot be negative
+        try:
+            val = self.len.simplify(fullsimplify=True)
+        except SimplifyException:
+            pass  # do not panic, it's simply a complex directive
+        else:
+            assert val.value >= 0, "Cannot insert a negative number of zeros"
+
+
+@dataclass(frozen=True)
+class JMP(Directive):
+    """Unconditional jump"""
+
+    dest: Param
+
+    def check(self) -> None:
+        self.dest.check()
+
+
+@dataclass(frozen=True)
+class INC(Directive):
+    """Increment"""
+
+    dest: Param
+
+    def check(self) -> None:
+        self.dest.check()
+        assert self.dest.can_be_written
+
+
+@dataclass(frozen=True)
+class DEC(Directive):
+    """Decrement"""
+
+    dest: Param
+
+    def check(self) -> None:
+        self.dest.check()
+        assert self.dest.can_be_written
+
+
+@dataclass(frozen=True)
+class MOV(Directive):
+    """Memory move"""
+
+    src: Param
+    dest: Param
+    size: Expression
+
+    def check(self) -> None:
+        for p in self.src, self.dest:
+            p.check()
+        assert self.dest.can_be_written
+        # size cannot be negative
+        try:
+            val = self.size.simplify(fullsimplify=True)
+        except SimplifyException:
+            pass  # do not panic, it's simply a complex directive
+        else:
+            assert val.value >= 0, "Cannot move a negative number of memory locations"
+
+
+@dataclass(frozen=True)
+class LOAD(Directive):
+    """Load from a pointer cell"""
+
+    src_ptr: Param
+    dest: Param
+
+    def check(self) -> None:
+        for p in self.src_ptr, self.dest:
+            p.check()
+        assert self.dest.can_be_written
+
+
+@dataclass(frozen=True)
+class STORE(Directive):
+    """Store to a pointer cell"""
+
+    src: Param
+    dest_ptr: Param
+
+    def check(self) -> None:
+        for p in self.src_ptr, self.dest:
+            p.check()
+
+
+@dataclass(frozen=True)
+class PUSH(Directive):
+    """Push a value onto the stack"""
+
+    value: Optional[Param]
+    size: Expression
+
+    def check(self) -> None:
+        if self.value is not None:
+            self.value.check()
+        # size cannot be negative
+        try:
+            val = self.size.simplify(fullsimplify=True)
+        except SimplifyException:
+            pass  # do not panic, it's simply a complex directive
+        else:
+            assert val.value >= 0, "Cannot push a negative number of memory locations"
+
+
+@dataclass(frozen=True)
+class POP(Directive):
+    """Pop a value from the stack"""
+
+    dest: Optional[Param]
+    size: Expression
+
+    def check(self) -> None:
+        if self.dest is not None:
+            self.dest.check()
+        assert self.dest.can_be_written
+        # size cannot be negative
+        try:
+            val = self.size.simplify(fullsimplify=True)
+        except SimplifyException:
+            pass  # do not panic, it's simply a complex directive
+        else:
+            assert val.value >= 0, "Cannot pop a negative number of memory locations"
+
+
+@dataclass(frozen=True)
+class CALL(Directive):
+    """Call a subroutine"""
+
+    dest: Param
+
+    def check(self) -> None:
+        self.dest.check()
+
+
+@dataclass(frozen=True)
+class RET(Directive):
+    """Return from a subroutine"""
+
+    pass
